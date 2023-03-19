7abcce67159086dbbdce32b5cfa9b3fe9f043fb7
Julia <odintsovaj22@gmail.com>
2023-03-19 17:51:41 +0300

    Created BottomNavigation

--- app/src/main/java/com/example/bu/MainActivity.kt
+++ app/src/main/java/com/example/bu/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.bu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.bu.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.homeFragment, R.id.productsFragment,
+            R.id.dishFragment, R.id.profileFragment
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
